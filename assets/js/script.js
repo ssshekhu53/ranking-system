e9b53f617ecc1bcb9786a7112ddf87b7ca893471
Shashank Shekhar <ssshekhu53@gmail.com>
2022-02-04 01:47:25 +0530

    Made OG ready and added check result button inside form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 let sno=1;
 
 $(document).ready(function() {
-    $('#lottery-form').append(participantBoxTemplate(sno++));
+    $('#lottery-form #form-container').append(participantBoxTemplate(sno++));
 });
 
 $(document).on('click', '.addBtn', function() {
-    $('#lottery-form').append(participantBoxTemplate(sno++));
+    $('#lottery-form #form-container').append(participantBoxTemplate(sno++));
     $('#lottery-form .participant-box:nth-last-child(1) input[name="name[]"]').focus();
     $('.removeBtn').attr('disabled', false);
 });
 
 $(document).on('click', '.removeBtn', function() {
     $($(this).attr('aria-boxid')).remove();
-    changeSerialNo('#lottery-form .participant-box');
+    changeSerialNo('#lottery-form #form-container .participant-box');
     sno--;
     if(sno<=2) 
         $('.removeBtn').attr('disabled', true);
 });
 
-$(document).on('click', '#checkResultBtn', function() {
+$(document).on('submit', '#lottery-form', function() {
     var participants=$('#lottery-form .participant-box').map(function() { return {name: $(this).find('input[name="name[]"]').val(), roll: $(this).find('input[name="roll[]"]').val()}; })
     var len=participants.length;
     var index=Math.floor(Math.random()*(len-1));
@@ -30,7 +30,7 @@ $(document).on('click', '#checkResultBtn', function() {
         imageUrl: 'assets/img/swal_icon.png',
         imageHeight: '25vh',
         title: 'Congo',
-        html: `<strong>${participants[index].name.toUpperCase()} (Roll. ${participants[index].roll})</strong> is the winner! Your fee will be paid by the committe.`,
+        html: `<strong>${participants[index].name.toUpperCase()} (Roll. ${participants[index].roll})</strong> is the winner! Your fee will be paid by your classmates.`,
         backdrop: 'rgba(0, 0, 0, 0.5) url(assets/img/party2.gif) repeat'
     });
 });
@@ -57,13 +57,13 @@ function participantBoxTemplate(sno) {
                     <div class="col-sm-8">
                         <div class="input-group mb-3">
                             <span class="input-group-text bg-dark bg-opacity-75 text-white fw-bold justify-content-center name-label" id="name${sno}">Name</span>
-                            <input type="text" class="form-control name-input" name="name[]" aria-describedby="name${sno}" />
+                            <input type="text" class="form-control name-input" name="name[]" aria-describedby="name${sno}" required />
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="input-group mb-3">
                             <span class="input-group-text bg-dark bg-opacity-75 text-white fw-bold justify-content-center roll-label" id="roll${sno}">Roll</span>
-                            <input type="text" class="form-control roll-input" name="roll[]" aria-describedby="roll${sno}" />
+                            <input type="text" class="form-control roll-input" name="roll[]" aria-describedby="roll${sno}" required />
                         </div>
                     </div>
                 </div>
