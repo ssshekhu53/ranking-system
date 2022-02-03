f7bab35b93b47810eb7a6434f93691016f17403f
Shashank Shekhar <ssshekhu53@gmail.com>
2022-02-04 00:30:29 +0530

    Create base UI and added participant box

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,61 @@ function expressionValidator(ch) {
 function valueValidator(ch) {
     return (parseInt(ch)>=0 && parseInt(ch)<=9);
 }
+
+let sno=1;
+
+$(document).ready(function() {
+    $('#lottery-form').append(participantBoxTemplate(sno++));
+});
+
+$(document).on('click', '.addBtn', function() {
+    $('#lottery-form').append(participantBoxTemplate(sno++));
+    $('.removeBtn').attr('disabled', false);
+});
+
+$(document).on('click', '.removeBtn', function() {
+    $($(this).attr('aria-boxid')).remove();
+    changeSerialNo('#lottery-form .participant-box');
+    sno--;
+    if(sno<=2) 
+        $('.removeBtn').attr('disabled', true);
+});
+
+function changeSerialNo(selector) {
+    let tempSno=1;
+    $(selector).each(function() {
+        $(this).attr('id', `participant-box-${tempSno}`);
+        $(this).find('.box-label').text(`Participant ${tempSno}`);
+        $(this).find('.name-label').attr('id', `name${tempSno}`);
+        $(this).find('.name-input').attr('id', `aria-describedby${tempSno}`);
+        $(this).find('.roll-label').attr('id', `name${tempSno}`);
+        $(this).find('.roll-input').attr('id', `aria-describedby${tempSno}`);
+        $(this).find('.removeBtn').attr('aria-boxid', `#participant-box-${tempSno}`);
+        tempSno++;
+    });
+}
+
+function participantBoxTemplate(sno) {
+    return `<div class="participant-box border border-1 border-dark px-4 py-3 mb-3" id="participant-box-${sno}">
+                <label class="fw-bold h5 text-muted mb-0 pb-0 box-label">Participant ${sno}</label>
+                <hr class="mt-0" />
+                <div class="row">
+                    <div class="col-sm-8">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text bg-dark bg-opacity-75 text-white fw-bold justify-content-center name-label" id="name${sno}">Name</span>
+                            <input type="text" class="form-control name-input" name="name[]" aria-describedby="name${sno}" />
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text bg-dark bg-opacity-75 text-white fw-bold justify-content-center roll-label" id="roll${sno}">Roll</span>
+                            <input type="text" class="form-control roll-input" name="roll[]" aria-describedby="roll${sno}" />
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-group w-100">
+                    <button type="button" class="btn btn-primary fw-bold addBtn">Add More</button>
+                    <button type="button" class="btn btn-outline-secondary fw-bold removeBtn" aria-boxid="#participant-box-${sno}" disabled>Remove</button>
+                </div>
+            </div>`;
+}
